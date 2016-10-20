afee76b6f4e5a602db89082180c19d5c6de59ae7
Ken Cao <ken.ccao@outlook.com>
2016-10-20 22:32:21 +0800

    emacs: org-mode: customized todo-status

--- emacs/emacs.org.el
+++ emacs/emacs.org.el
@@ -3,18 +3,27 @@
 ;;
 (require 'org-install)
 (require 'remember)
+
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
+
 ;;(define-key global-map "\C-ca" 'org-agenda)
 ;;(define-key global-map "\C-cl" 'org-store-link)
 ;;(setq org-log-done t)
 ;;
+
 (setq org-agenda-files (list "~/kcao/gtd-org/rh.org"
                              "~/tests/emacs-org/natural_p.org"))
 (setq org-default-notes-file "~/kcao/gtd-org/notes.org")
+
+(setq org-todo-keywords
+      '((sequence "REPORT(r)" "BUG(b)" "|" "FIXED(f)")
+        (sequence "TODO(t!)" "POSTPONED(p)" "|" "DONE(d@)" "CANCELED(c@/!)")))
+
 (setq org-remember-store-without-prompt nil)
 (setq org-remember-templates
  (quote ((116 "* TODO %? %u" "~/kcao/gtd-org/todo.org" "Tasks")
          (110 "* %u %?" "~/kcao/gtd-org/notes.org" "Notes"))))
+
 (setq remember-annotation-functions (quote (org-remember-annotation)))
 (setq remember-handler-functions (quote (org-remember-handler)))
 
